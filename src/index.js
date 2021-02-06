5b34a26382e9c977d9f7b4391534f8a88a359b05
DineshReddyKommera <dineshreddykommera@gmail.com>
2021-02-05 22:46:38 -0800

    Added Dependencies

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.js'
+import 'jquery/dist/jquery.js'
+import 'popper.js/dist/umd/popper.js'
+import 'bootstrap/dist/js/bootstrap.js'
 
 ReactDOM.render(
   <React.StrictMode>
